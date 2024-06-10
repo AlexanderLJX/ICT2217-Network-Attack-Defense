0268ed00ce023bfa0179fcde7d2a4a6db10f27e1
Gibaoson <100548863+Gibaoson@users.noreply.github.com>
2024-06-10 03:35:24 +0000

    Add files via upload

---
+++ sniffer_http.py
@@ -0,0 +1,23 @@
+from scapy.all import *
+import constants as c
+import os
+HTTP_PORT = [443,80]
+
+def content(pkt):
+	if pkt.haslayer(TCP) and pkt.src == getmacbyip(c.VICTIM_IP):
+		if pkt[TCP].dport in HTTP_PORT:
+			wrpcap("content.pcap",pkt,append=True)
+def checkfile(filename):
+	if os.path.exists(filename):
+		clear = input("Do you want to reset the pcap (Y|N): \t")
+		if clear.upper() == "Y":
+			os.remove(filename)
+
+def main():
+	# filename = input("File name?\t")
+	filename = "content.pcap"
+	checkfile(filename)
+	sniff(count=5,lfilter=content)
+
+if __name__ == "__main__":
+	main()
