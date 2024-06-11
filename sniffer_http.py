36f7282fb88633c8ef9224d3543e5baeaeefcf35
Gibaoson <100548863+Gibaoson@users.noreply.github.com>
2024-06-11 20:17:03 +0800

    revert

--- sniffer_http.py
+++ sniffer_http.py
@@ -1,17 +1,11 @@
 from scapy.all import *
 import constants as c
 import os
-
-# Function to get the MAC address for a given IP
-def get_mac(ip):
-    responses, unanswered = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip), timeout=2, retry=10)
-    for s, r in responses:
-        return r[Ether].src
-    return None
-
+HTTP_PORT = [443,80]
+VICTIM_SERVER_MAC = [getmacbyip(c.VICTIM_IP),getmacbyip(c.VICTIM_IP)]
 
 def content(pkt):
-	if pkt.haslayer(TCP) and pkt.src in VICTIM_SERVER_MAC:
+	if pkt.haslayer(TCP) and pkt.src in VICTIM_SERVER_MACfi:
 		if pkt[TCP].dport in HTTP_PORT:
 			wrpcap("content.pcap",pkt,append=True)
 def checkfile(filename):
@@ -25,9 +19,6 @@ def main():
 	filename = "content.pcap"
 	checkfile(filename)
 	sniff(count=5,lfilter=content)
-
-HTTP_PORT = [443,80]
-VICTIM_SERVER_MAC = [get_mac(c.VICTIM_IP),get_mac(c.VICTIM_IP)]
 
 if __name__ == "__main__":
 	main()
