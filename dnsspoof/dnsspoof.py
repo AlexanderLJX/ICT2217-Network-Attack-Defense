e1868362dffa070843a001d6b67b3a2c41be26ff
Alexander <83382087+AlexanderLJX@users.noreply.github.com>
2024-06-12 20:35:44 +0800

    Update dnsspoof.py

--- dnsspoof/dnsspoof.py
+++ dnsspoof/dnsspoof.py
@@ -17,7 +17,7 @@ def clone_website(url, save_dir):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
     conn = http.client.HTTPSConnection("example.com")
     response = conn.request("GET", "/", headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
